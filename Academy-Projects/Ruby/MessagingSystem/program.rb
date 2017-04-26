6eb6d757c65b445b589f4d777f0f3b6976c15cf6
ewaight12 <ewaight12@gmail.com>
2017-04-25 22:05:09 -0400

    Initial

--- Academy-Projects/Ruby/MessagingSystem/program.rb
+++
@@ -1,9 +0,0 @@
-#Old program
-require './inputs.rb'
-choice_input = Inputs.new
-choice_input.get_choice
-
-#Test Program
-# require './inputfaker.rb'
-# choice_input = Input_faker.new
-# choice_input.get_choice
